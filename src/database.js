ab5897f430625e89efe40919299b921034186b37
Gustavo Moreno <gusadolfo123@hotmail.com>
2019-10-17 02:02:22 +0000

    asd

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://gusadolfo123:gusadolfo123@cluster0-0pzn2.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://:@cluster0-0pzn2.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('db connected'))
